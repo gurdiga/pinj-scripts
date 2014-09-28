5df4ddf5b34fc506a8665d6b06846916d73bd0be
Vlad GURDIGA <gurdiga@gmail.com>
2014-09-28 18:50:14 +0300

    testing Mandrill

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict';
 
 var app = require('express')();
+var bodyParser = require('body-parser')
 
 app.use(require('./app/auth'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 require('./app/actions')(app);
 
 var port = process.env.PORT || 5000;
