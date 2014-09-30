afd8d110049f7149f53fa1ac8457e0d821949e3f
Vlad GURDIGA <gurdiga@gmail.com>
2014-09-30 10:52:04 +0300

    Housekeeping

--- app/auth.js
+++ app/auth.js
@@ -6,7 +6,5 @@ module.exports = function (req, res, next) {
 };
 
 function invalidAuth(req) {
-  console.log('pass', req.param('pass'));
-  console.log('KEY', process.env.KEY);
   return req.param('pass') !== process.env.KEY;
 }
