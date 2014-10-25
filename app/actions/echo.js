ffad39fc5e286a65b3661a02a5d54a458e0e31c6
Vlad GURDIGA <gurdiga@gmail.com>
2014-10-26 00:08:32 +0300

    echo: !!to == back

--- app/actions/echo.js
+++ app/actions/echo.js
@@ -1,9 +1,17 @@
 'use strict';
 
+var url = require('url');
+
 module.exports = function(req, res) {
   var to = req.param('to');
+  var referer = req.get('Referer');
 
-  if (to.substr(0, 1) === '/') to = req.get('Referer') + to;
+  if (!to || !referer) return res.redirect('back');
+
+  if (to.substr(0, 1) === '/') {
+    referer = url.parse(referer);
+    to = referer.protocol + '//' + referer.host + to;
+  }
 
   res.redirect(to);
 };
