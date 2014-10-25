008aa75c6c8b1884e0c737fdb93ca7e83622deca
Vlad GURDIGA <gurdiga@gmail.com>
2014-10-25 19:33:46 +0300

    public echo

--- app/auth.js
+++ app/auth.js
@@ -1,10 +1,17 @@
 'use strict';
 
 module.exports = function (req, res, next) {
+  if (isPublic(req.path)) next();
   if (invalidAuth(req)) res.status(401).end('Bad auth');
   else next();
 };
 
 function invalidAuth(req) {
   return req.param('pass') !== process.env.KEY;
+}
+
+isPublic['/echo'] = true;
+
+function isPublic(path) {
+  return isPublic[path];
 }
