6f3a0739246b698a79d6bd1336c0d858dc88e0f3
Vlad GURDIGA <gurdiga@gmail.com>
2014-10-25 23:47:26 +0300

    echo to

--- app/actions/echo.js
+++ app/actions/echo.js
@@ -1,5 +1,8 @@
 'use strict';
 
 module.exports = function(req, res) {
-  res.redirect('back');
+  var to = req.param('to');
+
+  if (to.substr(0, 1) === '/') to = req.get('Referer') + to;
+  res.redirect(to);
 };
