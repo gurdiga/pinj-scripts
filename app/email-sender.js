032f3e03a82e439c069f01a30246de54bb1d8ae8
Vlad GURDIGA <gurdiga@gmail.com>
2014-11-25 12:37:48 +0200

    Email sender: !secure !debug

--- app/email-sender.js
+++ app/email-sender.js
@@ -4,8 +4,6 @@ var SMTP_CONFIG = {
   'port':   process.env.SMTP_PORT,
   'host':   process.env.SMTP_HOST,
   'name':   process.env.SMTP_NAME,
-  'secure': process.env.SMTP_SECURE === 'true',
-  'debug':  process.env.SMTP_DEBUG === 'true',
   'auth': {
     'user': process.env.SMTP_USER,
     'pass': process.env.SMTP_PASS
